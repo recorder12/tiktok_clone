572573afb3a78ebd046e966c60585989f0bcbe20
Byeonggeon Kim <byeonggeon@naver.com>
2023-12-09 16:38:17 -0500

    login screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tiktok/constants/sign_up_screen.dart';
 
 void main() {
   runApp(const TikTokApp());
@@ -15,7 +16,7 @@ class TikTokApp extends StatelessWidget {
         primaryColor: const Color(0xFFE9435A),
         useMaterial3: true,
       ),
-      home: Container(),
+      home: const SignUpScreen(),
     );
   }
 }
